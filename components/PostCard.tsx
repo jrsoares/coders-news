f219aa653b281c96402fcf5beafcf36026ab57d6
Júnior Soares <juniorsoaresti@gmail.com>
2022-05-10 18:47:54 -0400

    Integrating with contentful

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,19 +1,25 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 import { useCallback } from "react";
+import { Post } from "../services/content";
 
-export default function PostCard({ post }) {
+type Props = {
+  post: Post;
+};
+export default function PostCard({ post }: Props) {
   const router = useRouter();
-  const handlePostClick = useCallback( () => {
-    router.push(`/post/${post.id}`
-    )
-  },[]);
+  const handlePostClick = useCallback(() => {
+    router.push(`/post/${post.sys.id}`);
+  }, []);
 
   return (
     <div className="w-[27.75rem]" onClick={handlePostClick}>
-      <img className="w-full h-60 object-cover rounded-[1rem] cursor-pointer mb-[1.25rem] "
-      src={post.thumbnail} alt="Post thumbnail" />
-      <h3 className="font-bold mb-[0.625rem] text-2xl">{post.title}</h3>
-      <span className='text-lg'>{post.description}</span>
+      <img
+        className="w-full h-60 object-cover rounded-[1rem] cursor-pointer mb-[1.25rem] hover:opacity-90"
+        src={post.fields.thumbnail.fields.file.url}
+        alt="Post thumbnail"
+      />
+      <h3 className="font-bold mb-[0.625rem] text-2xl">{post.fields.title}</h3>
+      <span className="text-lg">{post.fields.description}</span>
     </div>
-  )
+  );
 }
--- services/content.ts
+++ services/content.ts
@@ -1,18 +1,27 @@
-import Contentful, { createClient } from 'contentful'
-type Post = {
-  title: Contentful.EntryFields.Text
-  description: Contentful.EntryFields.Text
-  thumbnail: Contentful.Asset
-  content: Contentful.EntryFields.RichText
-}
+import { Document } from "@contentful/rich-text-types";
+import Contentful, { createClient, Entry } from "contentful";
+
+export type Post = Entry<{
+  title: Contentful.EntryFields.Text;
+  description: Contentful.EntryFields.Text;
+  thumbnail: Contentful.Asset;
+  content: Document;
+}>;
+
 const client = createClient({
-  space: 'yjv7xzqheiuf',
-  accessToken: '_JcwkY-A_0psSkr7SoEN_-fpfdac-uN-ualGQl54Yco'
-})
+  space: "yjv7xzqheiuf",
+  accessToken: "_JcwkY-A_0psSkr7SoEN_-fpfdac-uN-ualGQl54Yco"
+});
 
 export async function getPosts(): Promise<Contentful.Entry<Post>[]> {
-  const { items } = client.getEntries<Post>({
-    content_type: 'post'
-  })
-  return items
+  const { items } = await client.getEntries<Post>({
+    content_type: "post"
+  });
+
+  return items;
+}
+
+export async function getPost(postId: string): Promise<Contentful.Entry<Post>> {
+  const post = await client.getEntry<Post>(postId);
+  return post;
 }
